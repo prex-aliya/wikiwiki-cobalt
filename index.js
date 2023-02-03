96c776356f449d29769417b32f90ac8cae699fe3
prex-aliya <droved-through@proton.me>
2023-02-03 01:10:12 -0600

    🚀updated title change

--- index.js
+++ index.js
@@ -17,16 +17,23 @@ var t = setInterval(function () {
 
 function toggleTheme() {
         colorchange("white-text");
+
+        colorchange("white-head0");
+        colorchange("white-head1");
+        colorchange("white-head2");
+        colorchange("white-head3");
+        colorchange("white-head4");
+
         var element = document.body;
         element.classList.toggle("dark-mode");
         console.log("Toggled Theme");
 } 
 
 function colorchange(id) {
-    var background = document.getElementById(id).style.color;
-    if (background == "rgb(0, 0, 0)") {
-        document.getElementById(id).style.color = "#ffffff";
-    } else {
-        document.getElementById(id).style.color = "rgb(0,0,0)";
-    }
+        var background = document.getElementById(id).style.color;
+        if (background == "rgb(0, 0, 0)") {
+                document.getElementById(id).style.color = "#ffffff";
+        } else {
+                document.getElementById(id).style.color = "rgb(0,0,0)";
+        }
 }
